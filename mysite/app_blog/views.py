73a6868afa4a0dc2fef6309021cc6e4c4ede7fac
Igor Yurchenko <yurchiv@hotmail.com>
2021-06-07 12:27:28 +0300

    Lab 2 part 1

---
+++ mysite/app_blog/views.py
@@ -0,0 +1,8 @@
+# app_blog /views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Створіть свої представлення тут.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
