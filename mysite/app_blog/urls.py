c6e6cefc44ff94441afa4ad1ce97aa1ff4cadb6e
Igor Yurchenko <yurchiv@hotmail.com>
2021-08-16 17:37:22 +0300

    Lab 2 part 2

--- mysite/app_blog/urls.py
+++ mysite/app_blog/urls.py
@@ -1,6 +1,21 @@
 # app_blog/urls.py
 from django.urls import path
-from app_blog import views
+
+#from app_blog import views
+#urlpatterns = [
+#    path(r'', views.HomePageView.as_view()),
+#]
+
+
+from .views import (HomePageView, ArticleDetail,
+                    ArticleList, ArticleCategoryList)
 urlpatterns = [
-    path(r'', views.HomePageView.as_view()),
+    path(r'', HomePageView.as_view()),
+    path(r'articles', ArticleList.as_view(), name='articles-list'),
+    path(r'articles/category/<slug>',
+            ArticleCategoryList.as_view(),
+            name='articles-category-list'),
+    path(r'articles/<year>/<month>/<day>/<slug>',
+            ArticleDetail.as_view(),
+            name='news-detail'),
 ]
